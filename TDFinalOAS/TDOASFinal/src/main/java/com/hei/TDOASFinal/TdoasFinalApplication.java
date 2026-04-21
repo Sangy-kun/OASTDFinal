b2986126bb623221de67bd9169d63b27afcc0a28
Sangy-kun <rakotofif6@gmail.com>
2026-04-21 15:36:24 +0300

    api implementation

---
+++ TDFinalOAS/TDOASFinal/src/main/java/com/hei/TDOASFinal/TdoasFinalApplication.java
@@ -0,0 +1,13 @@
+package com.hei.TDOASFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdoasFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdoasFinalApplication.class, args);
+	}
+
+}
---
+++ TDFinalOAS/TDOASFinal/src/test/java/com/hei/TDOASFinal/TdoasFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.TDOASFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdoasFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
