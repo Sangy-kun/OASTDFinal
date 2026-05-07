f1f2b96ebacb3816dd2b05b109ae422970817a72
Sangy-kun <rakotofif6@gmail.com>
2026-05-07 11:45:50 +0300

    feat: security added for the api key

---
+++ TDFinalOAS/TDOASFinal/src/main/java/com/hei/TDOASFinal/security/ApiKeyFilter.java
@@ -0,0 +1,33 @@
+package com.hei.TDOASFinal.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class ApiKeyFilter extends OncePerRequestFilter {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String requestApiKey = request.getHeader("x-api-key");
+
+        if (apiKey.equals(requestApiKey)) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("text/plain");
+            response.getWriter().write("Bad credentials");
+        }
+    }
+}
